264b80bad661c6b4a522b27db8dbe3755a51eb6e
Sven Niederberger <s-niederberger@outlook.com>
2020-09-07 22:31:40 +0200

    .

--- fluid.py
+++ fluid.py
@@ -3,8 +3,7 @@ import pyglet as pg
 from pyglet.gl import *
 
 
-GRIDSIZEX = 128
-GRIDSIZEY = 256
+GRIDSIZE = 128
 WINDOW_SCALING_FACTOR = 3.
 GAUSS_SEIDEL_TOLERANCE = 1e-4
 
@@ -28,13 +27,15 @@ def set_bnd(b: int, x):
     x[-1, -1] = 0.5 * (x[-2, -1] + x[-1, -2])
 
 
-def lin_solve(b: int, x, x0, ax: float, ay: float, cx: float, cy: float):
+def lin_solve(b: int, x, x0, a: float, c: float):
     x_last = np.zeros_like(x)
     while True:
         x[1:-1, 1:-1] = (
-            x0[1:-1, 1:-1] + (ax * (x[0:-2, 1:-1] + x[2:, 1:-1]) +
-                              ay * (x[1:-1, 0:-2] + x[1:-1, 2:]))
-        ) / cx
+            x0[1:-1, 1:-1] + a * (x[0:-2, 1:-1] +
+                                  x[2:, 1:-1] +
+                                  x[1:-1, 0:-2] +
+                                  x[1:-1, 2:])
+        ) / c
         set_bnd(b, x)
 
         if np.max(np.abs(x_last-x)) < GAUSS_SEIDEL_TOLERANCE:
@@ -51,28 +52,26 @@ def diffuse(b: int, x, x0, diff: float, dt: float):
     """
     Spread density across neighboring cells.
     """
-    Nx = d.shape[0] - 2
-    Ny = d.shape[1] - 2
-    ax = dt * diff * Nx * Nx
-    ay = dt * diff * Ny * Ny
-    lin_solve(b, x, x0, ax, ay, 1. + 4. * ax, 1. + 4. * ay)
+    N = d.shape[0] - 2
+    a = dt * diff * N * N
+    lin_solve(b, x, x0, a, 1. + 4. * a)
 
 
 def advect(b: int, d, d0, u, v, dt: float):
     """
     Forces density to follow a given velocity field.
     """
-    Nx = d.shape[0] - 2
-    Ny = d.shape[1] - 2
+    N = d.shape[0] - 2
 
-    row_i = np.arange(1, Nx+1, dtype=np.intp).reshape(-1, 1)
-    col_j = np.arange(1, Ny+1, dtype=np.intp).reshape(1, -1)
+    row_i = np.arange(1, N+1, dtype=np.intp).reshape(-1, 1)
+    col_j = np.arange(1, N+1, dtype=np.intp).reshape(1, -1)
 
-    x = row_i - dt * Nx * u[1:-1, 1:-1]
-    y = col_j - dt * Ny * v[1:-1, 1:-1]
+    dt0 = dt * N
+    x = row_i - dt0 * u[1:-1, 1:-1]
+    y = col_j - dt0 * v[1:-1, 1:-1]
 
-    np.clip(x, 0.5, Nx + 0.5, out=x)
-    np.clip(y, 0.5, Ny + 0.5, out=y)
+    np.clip(x, 0.5, N + 0.5, out=x)
+    np.clip(y, 0.5, N + 0.5, out=y)
 
     i0 = x.astype(np.intp)
     j0 = y.astype(np.intp)
@@ -92,24 +91,24 @@ def advect(b: int, d, d0, u, v, dt: float):
 
 
 def project(u, v, p, div):
-    Nx = div.shape[0] - 2
-    Ny = div.shape[1] - 2
-    hx = 1. / Nx
-    hy = 1. / Ny
+    N = div.shape[0] - 2
+    h = 1. / N
 
-    div[1:-1, 1:-1] = -0.5 * \
-        (hx * (u[2:, 1:-1] - u[0:-2, 1:-1]) +
-         hy * (v[1:-1, 2:] - v[1:-1, 0:-2]))
+    div[1:-1, 1:-1] = -0.5 * h * \
+        (u[2:, 1:-1] -
+         u[0:-2, 1:-1] +
+         v[1:-1, 2:] -
+         v[1:-1, 0:-2])
 
     set_bnd(0, div)
 
     p[1:-1, 1:-1] = 0.
     set_bnd(0, p)
 
-    lin_solve(0, p, div, 1., 1., 4., 4.)
+    lin_solve(0, p, div, 1., 4.)
 
-    u[1:-1, 1:-1] -= 0.5 / hx * (p[2:, 1:-1] - p[0:-2, 1:-1])
-    v[1:-1, 1:-1] -= 0.5 / hy * (p[1:-1, 2:] - p[1:-1, 0:-2])
+    u[1:-1, 1:-1] -= 0.5 / h * (p[2:, 1:-1] - p[0:-2, 1:-1])
+    v[1:-1, 1:-1] -= 0.5 / h * (p[1:-1, 2:] - p[1:-1, 0:-2])
 
     set_bnd(1, u)
     set_bnd(2, v)
@@ -139,19 +138,18 @@ def vel_step(u, v, u0, v0, visc: float, dt: float):
 
 if __name__ == "__main__":
     # Velocity
-    v = np.zeros((GRIDSIZEX, GRIDSIZEY), dtype=np.float)
+    v = np.zeros((GRIDSIZE, GRIDSIZE), dtype=np.float)
     v_prev = v.copy()
-    u = np.zeros((GRIDSIZEX, GRIDSIZEY), dtype=np.float)
+    u = np.zeros((GRIDSIZE, GRIDSIZE), dtype=np.float)
     u_prev = u.copy()
 
     # Density
-    d = np.zeros((GRIDSIZEX, GRIDSIZEY), dtype=np.float)
+    d = np.zeros((GRIDSIZE, GRIDSIZE), dtype=np.float)
     d_prev = d.copy()
 
     # Drawing
-    WINDOW_SIZEX = int((GRIDSIZEX - 2) * WINDOW_SCALING_FACTOR)
-    WINDOW_SIZEY = int((GRIDSIZEY - 2) * WINDOW_SCALING_FACTOR)
-    window = pg.window.Window(WINDOW_SIZEX, WINDOW_SIZEY, caption="Fluid")
+    WINDOW_SIZE = int((GRIDSIZE-2) * WINDOW_SCALING_FACTOR)
+    window = pg.window.Window(WINDOW_SIZE, WINDOW_SIZE, caption="Fluid")
     window.config.alpha_size = 8
     fps_display = pg.window.FPSDisplay(window=window)
 
@@ -160,7 +158,7 @@ if __name__ == "__main__":
         coords /= WINDOW_SCALING_FACTOR
         coords += 1
         coords = coords.astype(np.intp)
-        coords = np.clip(coords, 1, (GRIDSIZEX-2, GRIDSIZEY-2))
+        coords = np.clip(coords, 1, GRIDSIZE-2)
         return coords[0], coords[1]
 
     @window.event
@@ -173,14 +171,14 @@ if __name__ == "__main__":
         data[1::4] = 255
         data[2::4] = 0
         data[3::4] = np.minimum(d[1:-1, 1:-1].T, 255).flatten()
-        image = pg.image.ImageData(d.shape[0] - 2, d.shape[1] - 2,
+        image = pg.image.ImageData(d.shape[0]-2, d.shape[1]-2,
                                    'RGBA', data.tobytes())
 
         window.clear()
 
         image.blit(x=0, y=0,
-                   width=WINDOW_SIZEX,
-                   height=WINDOW_SIZEY)
+                   width=WINDOW_SIZE,
+                   height=WINDOW_SIZE)
 
         fps_display.draw()
 
@@ -205,8 +203,8 @@ if __name__ == "__main__":
         u_prev.fill(0.)
         v_prev.fill(0.)
 
-        d_prev[GRIDSIZEX//2, GRIDSIZEY//16] = 10000.
-        v_prev[GRIDSIZEX//2, GRIDSIZEY//16] = 50.
+        d_prev[GRIDSIZE//2, GRIDSIZE//16] = 10000.
+        v_prev[GRIDSIZE//2, GRIDSIZE//16] = 50.
 
     pg.clock.schedule_interval(update, 1/FPS)
     pg.app.run()
